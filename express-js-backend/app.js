22e03752465fd0c506f5898521c1c8ca9f54e5c5
dylanbrass <dylan.brassard@outlook.com>
2024-02-04 18:36:11 -0500

    Fixing be and started book feature

--- express-js-backend/app.js
+++ express-js-backend/app.js
@@ -1,5 +1,4 @@
 import('./server/loadEnv.mjs');
-import('./templates/confirmCommendation.handlebars')
 const express = require('express');
 const cors = require('cors')
 const path = require('path');
