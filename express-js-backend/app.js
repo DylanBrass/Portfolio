e7286747a55e38763bcc6617bd1ed1023e6b2db3
dylanbrass <dylan.brassard@outlook.com>
2024-02-04 19:09:49 -0500

    Changed vercel.json

--- express-js-backend/app.js
+++ express-js-backend/app.js
@@ -24,12 +24,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'templates')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
-
-
-
 
 
 app.use(cors())
