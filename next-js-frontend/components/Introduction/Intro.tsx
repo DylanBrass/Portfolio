e27db970309287003d3267841f9076d2a2de9839
dylanbrass <dylan.brassard@outlook.com>
2024-02-20 11:54:57 -0500

    Fixed a few secret leaked

--- next-js-frontend/components/Introduction/Intro.tsx
+++ next-js-frontend/components/Introduction/Intro.tsx
@@ -6,16 +6,16 @@ export default function Intro() {
 
 
     return (
-        <div className={"h-max"}>
+        <div className={"h-max w-screen"}>
             <div className={"flex flex-col justify-center items-center h-full"}>
                 <h1 className={"text-2xl sm:text-6xl font-bold text-center text-white"}>Welcome to my portfolio</h1>
                 <h2 className={"text-1xl sm:text-4xl  font-bold text-center text-white"}>&lt;Full Stack Developer /&gt;</h2>
-                <div className={"flex flex-row text-center"}>
+                <div className={"flex-row text-center"}>
                     <a href={"/projects"}
-                       className={"mt-10 bg-white hover:bg-red-300 text-black font-bold py-2 px-4 mx-20 rounded"}>See my
+                       className={"mt-10 bg-white hover:bg-red-300 text-black font-bold py-2 px-4 sm:mx-20 rounded"}>See my
                         projects
                     </a>
-                    <a className={"mt-10 bg-white hover:bg-red-300 text-black font-bold py-2 px-4 mx-20 rounded"}
+                    <a className={"mt-10 bg-white hover:bg-red-300 text-black font-bold py-2 px-4 sm:mx-20 rounded"}
                        href="/CV_fr.pdf" download={"dylan_brassard_cv"}>Download my
                         resume
                     </a>
